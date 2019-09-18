e354aca45c9567a39192b17bf74709583fce336a
AlekseyMikheev <dagas_88@mail.ru>
2019-09-18 19:40:46 +0300

    Add setup.js

--- js/setup.js
+++ js/setup.js
@@ -2,81 +2,96 @@
 
 
 var NAMES = [
-    'Иван',
-    'Хуан Себастьян',
-    'Мария',
-    'Кристоф',
-    'Виктор',
-    'Юлия',
-    'Люпита',
-    'Вашингтон'
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон'
 ];
 
 var SURNAMES = [
-    'да Марья',
-    'Верон',
-    'Мирабелла',
-    'Вальц',
-    'Онопко',
-    'Топольницкая',
-    'Нионго',
-    'Ирвинг'
+  'да Марья',
+  'Верон',
+  'Мирабелла',
+  'Вальц',
+  'Онопко',
+  'Топольницкая',
+  'Нионго',
+  'Ирвинг'
 ];
 
 var COATCOLORS = [
-    'rgb (101, 137, 164)',
-    'rgb (241, 43, 107)',
-    'rgb (146, 100, 161)',
-    'rgb (56, 159, 117)',
-    'rgb (215, 210, 55)',
-    'rgb (0, 0, 0)'
-]
+  'rgb(101,137,164)',
+  'rgb(241,43,107)',
+  'rgb(146,100,161)',
+  'rgb(56,159,117)',
+  'rgb(215,210,55)',
+  'rgb(0,0,0)'
+];
 
 var EYESCOLORS = [
-    'black',
-    'red',
-    'blue',
-    'yellow',
-    'green'
-]
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
+];
 
-function randomInteger(min, max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    return Math.round(rand);
-}
+var randomInteger = function (min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.abs(Math.round(rand));
+};
 
 var getRandomString = function (strings) {
-    var randomIndex = randomInteger(0, strings.length);
-    return strings[randomIndex];
-}
+  var randomIndex = randomInteger(0, strings.length);
+  return strings[randomIndex];
+};
 
-var getRandomItem = function (strings) {
-    var item = {};
+var getRandomItem = function () {
+  var item = {};
 
-    item.name = `${getRandomString(NAMES)} ${getRandomString(SURNAMES)}`;
+  item.name = getRandomString(NAMES) + ' ' + getRandomString(SURNAMES);
+  item.coatColor = getRandomString(COATCOLORS);
+  item.eyesColor = getRandomString(EYESCOLORS);
 
-    item.coatColor = getRandomString(COATCOLORS);
-    item.eyesColor = getRandomString(EYESCOLORS);
-
-    return item;
-}
+  return item;
+};
 
 var getRandomItems = function (count) {
-    var items = [];
-    for (var i = 0; i < count; i++) {
-        var item = getRandomItem();
-        items.push(item);
-    }
-    return items;
-}
+  var items = [];
+  for (var i = 0; i < count; i++) {
+    var item = getRandomItem();
+    items.push(item);
+  }
+  return items;
+};
+
+var createDOMItem = function (item, template) {
+  template.querySelector('.setup-similar-label').textContent = item.name;
+  template.querySelector('.wizard-coat').style.fill = item.coatColor;
+  template.querySelector('.wizard-eyes').style.fill = item.eyesColor;
+
+  return template;
+};
+
+var bindDOMItems = function (items) {
+  var itemTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+  for (var i = 0; i < items.length; i++) {
+    var domItem = createDOMItem(items[i], itemTemplate.cloneNode(true));
+    dialogList.appendChild(domItem);
+  }
+};
+
+var dialog = document.querySelector('.setup');
+dialog.classList.remove('hidden');
+
+document.querySelector('.setup-similar').classList.remove('hidden');
+
+var dialogList = document.querySelector('.setup-similar-list');
 
 var items = getRandomItems(4);
 
-console.log(items)
-
-
-var setup = document.querySelector('.setup');
-
-if (setup != null) {
-    setup.classList.remove('hidden');
-}
+bindDOMItems(items);
