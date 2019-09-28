a8eb49dd7312eea041e7c09b2a9339ce2a74d5eb
AlekseyMikheev <dagas_88@mail.ru>
2019-09-28 07:46:38 +0300

    Add drag dialog window.

--- js/dialog.js
+++ js/dialog.js
@@ -10,6 +10,8 @@ window.dialog = (function () {
 
   var openDialog = function () {
     setupDialogOpened = true;
+    setupDialog.style.left = setupDialogCoords.x;
+    setupDialog.style.top = setupDialogCoords.y;
     setupDialog.classList.remove('hidden');
   };
 
@@ -56,6 +58,53 @@ window.dialog = (function () {
         closeDialog();
       }
     });
+
+    dialogHandler.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+
+      var dragged = false;
+
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        dragged = true;
+
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
+        setupDialog.style.top = (setupDialog.offsetTop - shift.y) + 'px';
+        setupDialog.style.left = (setupDialog.offsetLeft - shift.x) + 'px';
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+
+        if (dragged) {
+          var onClickPreventDefault = function (clickEvt) {
+            clickEvt.preventDefault();
+            dialogHandler.removeEventListener('click', onClickPreventDefault);
+          };
+          dialogHandler.addEventListener('click', onClickPreventDefault);
+        }
+      };
+
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
   };
 
   var bindWizardEvent = function (wizardPart, property, colors, wizardPartHidden) {
@@ -78,8 +127,13 @@ window.dialog = (function () {
   };
 
   var setupDialog = document.querySelector('.setup');
+  var setupDialogCoords = {
+    x: setupDialog.style.left,
+    y: setupDialog.style.top
+  };
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setupDialog.querySelector('.setup-close');
+  var dialogHandler = setupDialog.querySelector('.upload');
 
   initDocumentEvents();
 
